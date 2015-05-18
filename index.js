5293d9b89fda67763e040641ed895e4744f861fb
Daniel Cousens <github@dcousens.com>
2015-05-18 13:41:46 +1000

    add impl. and test fixtures

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = {}
+function classicSort(a, b) {
+  return a - b
+}
+
+module.exports = function checksort(array, compare) {
+  compare = compare || classicSort
+
+  for (var i = 1; i < array.length; ++i) {
+    var a = array[i - 1]
+    var b = array[i]
+
+    if (compare(a, b) !== -1) return false
+  }
+
+  return true
+}
