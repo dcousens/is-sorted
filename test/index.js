a0dc1a006b00189f04481c429ec1e78eb45c7b3d
Daniel Cousens <dcousens@users.noreply.github.com>
2015-08-06 14:19:04 +1000

    tests: standard styling

--- test/index.js
+++ test/index.js
@@ -4,12 +4,12 @@ var assert = require('assert')
 var sorted = require('../')
 var fixtures = require('./fixtures')
 var comparators = {
-  descending: function(a, b) { return b - a }
+  descending: function (a, b) { return b - a }
 }
 
-describe('sorted', function() {
-  fixtures.forEach(function(f) {
-    it('returns ' + f.expected + ' for ' + f.array, function() {
+describe('sorted', function () {
+  fixtures.forEach(function (f) {
+    it('returns ' + f.expected + ' for ' + f.array, function () {
       var actual = sorted(f.array, comparators[f.comparator])
 
       assert.strictEqual(actual, f.expected)
