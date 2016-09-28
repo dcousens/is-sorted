f23fe33b89ea571f22a449a6c43a17b437a7ac7d
Daniel Cousens <dcousens@users.noreply.github.com>
2016-09-28 18:09:24 +1000

    rename to comparator

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function defaultSort (a, b) {
   return a - b
 }
 
-module.exports = function checksort (array, compare) {
-  compare = compare || defaultSort
+module.exports = function checksort (array, comparator) {
+  comparator = comparator || defaultSort
 
   for (var i = 1; i < array.length; ++i) {
     var a = array[i - 1]
     var b = array[i]
 
-    if (compare(a, b) > 0) return false
+    if (comparator(a, b) > 0) return false
   }
 
   return true
