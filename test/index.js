1ad3e9e9aa1e7be8c7c7b3c3fe6c013facca57c5
Daniel Cousens <github@dcousens.com>
2017-09-25 16:37:47 +1000

    use tape, update travis

--- test/index.js
+++ test/index.js
@@ -1,18 +1,15 @@
-/* global describe, it */
-
 var assert = require('assert')
 var sorted = require('../')
 var fixtures = require('./fixtures')
+var tape = require('tape')
 var comparators = {
   descending: function (a, b) { return b - a }
 }
 
-describe('sorted', function () {
-  fixtures.forEach(function (f) {
-    it('returns ' + f.expected + ' for ' + f.array, function () {
-      var actual = sorted(f.array, comparators[f.comparator])
+fixtures.forEach(function (f) {
+  tape('returns ' + f.expected + ' for ' + f.array, function () {
+    var actual = sorted(f.array, comparators[f.comparator])
 
-      assert.strictEqual(actual, f.expected)
-    })
+    assert.strictEqual(actual, f.expected)
   })
 })
