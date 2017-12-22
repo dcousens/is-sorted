bb4e84885d53d4608ba096bb82bc4a2706f46a25
xiuxingderensheng <543621754@qq.com>
2017-12-22 09:55:30 +0800

    storage array.length

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function defaultComparator (a, b) {
 module.exports = function checksort (array, comparator) {
   comparator = comparator || defaultComparator
 
-  for (var i = 1; i < array.length; ++i) {
+  for (var i = 1, length = array.length; i < length; ++i) {
     if (comparator(array[i - 1], array[i]) > 0) return false
   }
 
