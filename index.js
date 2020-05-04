3440d4d19c7d09f2fe00aac5b0d7dc6f8db47057
qinsfu <44136546+qxdi@users.noreply.github.com>
2020-05-04 11:18:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-function defaultComparator (a, b) {
-  return a - b
-}
-
 module.exports = function checksort (array, comparator) {
-  if (!Array.isArray(array)) throw new TypeError('Expected Array, got ' + (typeof array))
-  comparator = comparator || defaultComparator
-
-  for (var i = 1, length = array.length; i < length; ++i) {
-    if (comparator(array[i - 1], array[i]) > 0) return false
-  }
-
-  return true
+    if(!Array.isArray(array)) throw new TypeError('Expected Array, got ' + (typeof array));
+    const defaultComparator = (a, b) => a < b;
+    
+    comparator = comparator || defaultComparator;
+    const illegal = array.find((i, index) => {
+        if(index === array.length - 1) return false;
+        return !comparator(array[index], array[index+1]);
+    });
+    return illegal === undefined;
 }
