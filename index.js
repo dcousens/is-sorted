aad954163e80a28a9fb9ed10397c3ed8b78be4d1
Muhammad Faizan Uddin <faizan.uddin94@gmail.com>
2021-06-09 15:59:23 +0400

    Update index.js
    Added default comparator

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function defaultComparator (a, b) {
-  return a - b
-}
-
-module.exports = function checksort (array, comparator) {
+module.exports = function checksort (array, comparator = (a,b)=>a-b) {
   if (!Array.isArray(array)) throw new TypeError('Expected Array, got ' + (typeof array))
-  comparator = comparator || defaultComparator
 
   for (var i = 1, length = array.length; i < length; ++i) {
     if (comparator(array[i - 1], array[i]) > 0) return false
